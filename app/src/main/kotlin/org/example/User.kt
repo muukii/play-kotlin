5b34c7d1dca94330d36c7bacabf18acb0db3c727
Muukii <muukii.app@gmail.com>
2024-04-10 20:59:32 +0900

    init

---
+++ app/src/main/kotlin/org/example/User.kt
@@ -0,0 +1,19 @@
+package org.example
+
+class User(name: String) {
+//    var name: String
+
+    //    constructor(name: String) {
+//        println("constructor")
+//        if (name == "") {
+//            this.name = "empty"
+//        } else {
+//            this.name = name
+//        }
+//    }
+//
+
+    init {
+        println(name)
+    }
+}
